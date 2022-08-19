8f01cbf9c62c8b97960fd300634cd7ed9f6f3a1b
unknown <ykhokhaneshiy@gmail.com>
2022-08-19 09:42:52 +0530

    server.js - PORT updated

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ const router = require("./server/routes/apiRouter");
 const { sendMailJob } = require('./server/jobs/sendMailJob');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.use(express.json());
 
 app.use("/", router);
 
-app.listen(3000, () => {
-  console.log("Server running!");
+app.listen(PORT, () => {
+  console.log(`Server running! on ${PORT}`);
 
   //scheduling job for sending mail every minute
   schedule.scheduleJob('*/1 * * * *', sendMailJob);
